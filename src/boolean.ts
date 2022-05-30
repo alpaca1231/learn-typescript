c901533fe6fa4797a1ca17d32b222ed307582c7d
alpaca1231 <kazuki.amatatsu.fd@gmail.com>
2022-05-31 00:10:41 +0900

    boolean型 null型 undefined型

---
+++ src/boolean.ts
@@ -0,0 +1,16 @@
+/**
+ * boolean型
+ * 真偽値リテラル
+ * true, falseは値であり、予約語でもある
+ */
+
+const yes: boolean = true
+const no: boolean = false
+
+/**
+ * null型 undefined型
+ * nullはリテラルであり、undefinedは変数名という扱いである
+ * どちらもデータがないという状況を表す
+ */
+const n: null = null
+const u: undefined = undefined
